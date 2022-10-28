9aee0497bd380ea89c9474f07c2587715b561309
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-10-28 10:03:41 +0200

    correzzione utility

---
+++ connection_utility.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+class MySql:
+
+    def __init__(self):
+        try:
+            self.conn = mysql.connector.connect(host='localhost', database='sakila', user='root', password='Snake.1234')
+            self.cursor = self.conn.cursor()
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+
+    def close_connection(self):
+        if (self.conn is not None):
+                self.cursor.close()
+                self.conn.close()
+                print("MySQL connection is closed")
+
+    def execute(self, sql):
+        self.cursor.execute(sql)
+
+    def fetchall(self):
+        return self.cursor.fetchall()
