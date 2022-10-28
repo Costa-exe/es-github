324e82786294e908da14e138786232c6ab76c4f0
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-10-28 11:33:01 +0200

    modifiche a film di dev 1 - traccia 2

--- film.py
+++ film.py
@@ -9,6 +9,15 @@ class Film:
 
         return all_film
 
+    def getFilmByTitle(self, title):
+        d = MySql()
+        d.execute(f"select * from film where title = '{title}'")
+        film_by_title = d.fetchall()
+        d.close_connection()
+
+        return film_by_title
+
 film = Film()
 
 print(film.getAllFilm())
+print(film.getFilmByTitle('ACE GOLDFINGER'))
